2545315a0d3587606044d4d84694fa5287daff27
Dog2puppy <colecooper2005@icloud.com>
2017-03-03 00:23:53 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Dog2puppy/PocketMineAuth/Main.php
+++ src/Dog2puppy/PocketMineAuth/Main.php
@@ -1,21 +1,29 @@
 <?php
 
-use pocketmine\plugin\PluginBase;
+/*
+ * _____           _        _   __  __ _                          _   _
+ *  |  __ \         | |      | | |  \/  (_)              /\        | | | |
+ *  | |__) |__   ___| | _____| |_| \  / |_ _ __   ___   /  \  _   _| |_| |__
+ *  |  ___/ _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \ / /\ \| | | | __| '_ \
+ *  | |  | (_) | (__|   <  __/ |_| |  | | | | | |  __// ____ \ |_| | |_| | | |
+ *  |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___/_/    \_\__,_|\__|_| |_|
+ *
+ *
+ * License: dog2puppy.mit-license.org
+ */
+
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
 
-Class Main extends PluginBase implements Listener {
-
-  public function onEnable(){
-    $config = new Config($this->getDataFolder().'config.yml', Config::YAML);
-    $mysql = new mysqli($config->get('mysql')['host'], $config->get('mysql')['name'], $config->get('mysql')['password'], $config->get('mysql')['database']);
-    if ($config->get('firstrun') == true) {
-      
-      $config->set('firstrun', false);
+class Main extends PluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $config = new Config($this->getDataFolder().'config.yml', Config::YAML);
+        $mysql = new mysqli($config->get('mysql')['host'], $config->get('mysql')['name'], $config->get('mysql')['password'], $config->get('mysql')['database']);
+        if ($config->get('firstrun') == true) {
+            $config->set('firstrun', false);
+        }
     }
-  }
-
 }
